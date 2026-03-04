baf56295d7f1814307fd3f99074c5b2d5142ee4c
Shubham Kushwah <sam.kushwah.2001@gmail.com>
2026-03-04 11:28:55 +0530

    added login endpoints ,genrated jwt tokedn and tested on jwt.io

--- journal-api/src/main/java/com/shubham/journal_api/controller/AuthController.java
+++ journal-api/src/main/java/com/shubham/journal_api/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.shubham.journal_api.controller;
 
 import com.shubham.journal_api.dto.AuthResponse;
+import com.shubham.journal_api.dto.LoginRequest;
 import com.shubham.journal_api.dto.RegisterRequest;
 import com.shubham.journal_api.service.AuthService;
 import jakarta.validation.Valid;
@@ -8,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.annotation.RequestScope;
+
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,6 +27,19 @@ public class AuthController {
             return new ResponseEntity<>(
                     new AuthResponse(e.getMessage(), null),
             HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
+        try{
+            AuthResponse response = authService.login(request);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(
+                    new AuthResponse(e.getMessage(),null),
+                    HttpStatus.UNAUTHORIZED
             );
         }
     }
--- journal-api/src/main/java/com/shubham/journal_api/service/AuthService.java
+++ journal-api/src/main/java/com/shubham/journal_api/service/AuthService.java
@@ -1,9 +1,11 @@
 package com.shubham.journal_api.service;
 
 import com.shubham.journal_api.dto.AuthResponse;
+import com.shubham.journal_api.dto.LoginRequest;
 import com.shubham.journal_api.dto.RegisterRequest;
 import com.shubham.journal_api.model.User;
 import com.shubham.journal_api.repository.UserRepository;
+import com.shubham.journal_api.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,9 @@ public class AuthService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     public AuthResponse register(RegisterRequest request){
         //check if username already exists
@@ -42,5 +47,24 @@ public class AuthService {
         userRepository.save(user);
 
         return new AuthResponse("User registerd successfully ",user.getUsername());
+    }
+
+    //Login method
+    public AuthResponse login(LoginRequest request){
+        //find user by username
+        User user= userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("Invalid username or password"));
+
+        //verify password
+        //psswordEncoder.matches(plainPassword, hashedPassword)
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid username or password");
+        }
+
+        //Generate JWT token
+        String token = jwtUtil.generateToken(user.getUsername());
+
+        //Return response with token
+        return new AuthResponse("Login successfull", user.getUsername(),token);
     }
 }
